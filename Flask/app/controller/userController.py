25baffb0c6a5cc52fefcde992bf045142a52a281
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-15 04:26:15 +0300

    Update userController.py

--- Flask/app/controller/userController.py
+++ Flask/app/controller/userController.py
@@ -1,37 +1,36 @@
 from flask import Blueprint
 
 user_blueprint = Blueprint('user', __name__)
+from app.service.userService import *
 
-def demo(id=10):
-    return 'demo',200
 
 
 # Admin role
 
 #   Get all users
-user_blueprint.route('/all',methods=['GET'])(demo)
+user_blueprint.route('/all',methods=['GET'])(get_all_users)
 
 #   Get user by id
-user_blueprint.route('/<int:id>',methods=['GET'])(demo)
+user_blueprint.route('/<int:id>',methods=['GET'])(get_user_by_id)
 
 #   Update user by id
-user_blueprint.route('/<int:id>',methods=['PUT'])(demo)
+user_blueprint.route('/<int:id>',methods=['PUT'])(update_user_by_id)
 
 #   Delete user by id
-user_blueprint.route('/<int:id>',methods=['DELETE'])(demo)
+user_blueprint.route('/<int:id>',methods=['DELETE'])(delete_user_by_id)
 
 
 
 # any User role
 
 #   Get user by using authenticated user
-user_blueprint.route('/',methods=['GET'])(demo)
+user_blueprint.route('/',methods=['GET'])(get_me)
 
 #   Update user by using authenticated user
-user_blueprint.route('/',methods=['PUT'])(demo)
+user_blueprint.route('/',methods=['PUT'])(update_me)
 
 #   Delete user by using authenticated user
-user_blueprint.route('/',methods=['DELETE'])(demo)
+user_blueprint.route('/',methods=['DELETE'])(delete_me)
 
 
 
@@ -39,10 +38,10 @@ user_blueprint.route('/',methods=['DELETE'])(demo)
 # APIS without role
 
 #   Login user
-user_blueprint.route('/login',methods=['POST'])(demo)
+user_blueprint.route('/login',methods=['POST'])(login)
 
 #   add new user
-user_blueprint.route('/',methods=['POST'])(demo)
+user_blueprint.route('/',methods=['POST'])(add_user)
 
 
 
