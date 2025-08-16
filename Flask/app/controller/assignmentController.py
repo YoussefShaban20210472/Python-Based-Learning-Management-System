d37b8fc72f3373402e224910217f31843146a143
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:01:57 +0300

    Update assignmentController.py

--- Flask/app/controller/assignmentController.py
+++ Flask/app/controller/assignmentController.py
@@ -1,35 +1,23 @@
 from flask import Blueprint
-
+from app.service.assignmentService import *
 assignment_blueprint = Blueprint('assignment', __name__)
 
-def demo(course_id=0,id=0):
-    return 'demo'
 
 
-
-# Instructor role
 
 #   Add new assignment
-assignment_blueprint.route('/',methods=['POST'])(demo)
+assignment_blueprint.route('/',methods=['POST'])(add_assignment)
 
 #   Get assignment by id
-assignment_blueprint.route('/<int:id>',methods=['GET'])(demo)
+assignment_blueprint.route('/<int:assignment_id>',methods=['GET'])(get_assignment_by_id)
 
 #   Get all assignments
-assignment_blueprint.route('/all',methods=['GET'])(demo)
+assignment_blueprint.route('/all',methods=['GET'])(get_all_assignments)
 
 #   Update assignment by id
-assignment_blueprint.route('/<int:id>',methods=['PUT'])(demo)
+assignment_blueprint.route('/<int:assignment_id>',methods=['PUT'])(update_assignment_by_id)
 
 #   Delete assignment by id
-assignment_blueprint.route('/<int:id>',methods=['DELETE'])(demo)
+assignment_blueprint.route('/<int:assignment_id>',methods=['DELETE'])(delete_assignment_by_id)
 
-
-# Student role
-
-#   Get assignment by id
-assignment_blueprint.route('/<int:id>',methods=['GET'])(demo)
-
-#   Get all assignments
-assignment_blueprint.route('/all',methods=['GET'])(demo)
 
