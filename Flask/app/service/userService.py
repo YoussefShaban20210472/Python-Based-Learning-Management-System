8edfe51d81b1a74dad4d98b7d6fd93d70f46023a
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:06:51 +0300

    Update userService.py

--- Flask/app/service/userService.py
+++ Flask/app/service/userService.py
@@ -1,57 +1,49 @@
 from flask import  request
-from app.Repository import  userRepository
+from app.Repository.Repository import  execute
+import inspect
+
+
 def add_user():
     data = request.get_json()
-    print(data)
-    return userRepository.add_user(data)
+    return execute(inspect.currentframe().f_code.co_name,data)
 
 def login():
     data = request.get_json()
-    print(data)
-
-    return userRepository.login(data)
+    return execute(inspect.currentframe().f_code.co_name, data)
 
 
 
 def get_all_users():
     data = {'actor_id': getattr(request, "actor_id", None)}
-    print(data)
+    return execute(inspect.currentframe().f_code.co_name, data)
 
-    return userRepository.get_all_users(data)
-
-def get_user_by_id(id):
-    data = {'actor_id': getattr(request, "actor_id", None),'target_id': id}
-    print(data)
-    return userRepository.get_user_by_id(data)
+def get_user_by_id(user_id):
+    data = {'actor_id': getattr(request, "actor_id", None),'target_id': user_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
 
 
-def update_user_by_id(id):
+def update_user_by_id(user_id):
     data = request.get_json()
-    data.update({'actor_id': getattr(request, "actor_id", None), 'target_id': id})
-    print(data)
-    return userRepository.update_user_by_id(data)
+    data.update({'actor_id': getattr(request, "actor_id", None), 'target_id': user_id})
+    return execute(inspect.currentframe().f_code.co_name, data)
 
-def delete_user_by_id(id):
-    data = {'actor_id': getattr(request, "actor_id", None),'target_id': id}
-    print(data)
-    return userRepository.delete_user_by_id(data)
+def delete_user_by_id(user_id):
+    data = {'actor_id': getattr(request, "actor_id", None),'target_id': user_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
 
 
 def get_me():
     data = {'actor_id': getattr(request, "actor_id", None), 'target_id': getattr(request, "actor_id", None)}
-    print(data)
-    return userRepository.get_user_by_id(data)
+    return execute('get_user_by_id', data)
 
 def update_me():
     data = request.get_json()
     data.update({'actor_id': getattr(request, "actor_id", None), 'target_id': getattr(request, "actor_id", None)})
-    print(data)
-    return userRepository.update_user_by_id(data)
+    return execute('update_user_by_id', data)
 
 def delete_me():
     data = {'actor_id': getattr(request, "actor_id", None),'target_id': getattr(request, "actor_id", None)}
-    print(data)
-    return userRepository.delete_user_by_id(data)
+    return execute('delete_user_by_id', data)
 
 
 
