be3f8cf2fe2585da7e88e71a507bcd12ad13a923
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:05:23 +0300

    Create mediaFileService.py

---
+++ Flask/app/service/mediaFileService.py
@@ -0,0 +1,28 @@
+from flask import  request
+from app.Repository.Repository import  execute
+import inspect
+
+#   Add new media file
+def add_media_file(course_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Get media file by id
+def get_media_file_by_id(media_file_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Get all media files
+def get_all_media_files(course_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Delete media file by id
+def delete_media_file_by_id(media_file_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+
+
+
+
