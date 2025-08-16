22b277432cd2e2c5074463e3a3fc5aa219df55b6
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 15:20:44 +0300

    Update attendanceService.py

--- Flask/app/service/attendanceService.py
+++ Flask/app/service/attendanceService.py
@@ -3,6 +3,12 @@ from flask import  request
 from app.Repository.Repository import  execute
 import inspect
 
+#   Attend attendance
+def add_attendance(lesson_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'lesson_id': lesson_id})
+    return execute(inspect.currentframe().f_code.co_name, data)
+
 
 #   Get attendance by id
 def get_attendance_by_id(attendance_id):
@@ -15,13 +21,6 @@ def get_all_attendances(course_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
-
-
-
-#   Attend attendance
-def add_attendance(lesson_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'lesson_id': lesson_id}
-    return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get attendance by using authenticated student
 def get_my_attendance(lesson_id):
