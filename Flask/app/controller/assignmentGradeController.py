df8090447ee97f14dcca412cb18b8acaa94d14f9
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 15:20:21 +0300

    Update assignmentGradeController.py

--- Flask/app/controller/assignmentGradeController.py
+++ Flask/app/controller/assignmentGradeController.py
@@ -6,24 +6,24 @@ assignment_grade_blueprint = Blueprint('assignment_grade', __name__)
 # Instructor role
 
 #   Add assignment grade
-assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['POST'])(add_assignment_submission_grade)
+assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['POST'])(add_submission_grade)
 
 #   Get assignment grade by id
-assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['GET'])(get_assignment_submission_grade)
+assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['GET'])(get_submission_grade_by_id)
 
-#   Get assignment grades
-assignment_grade_blueprint.route('/all/grade',methods=['GET'])(get_assignment_submissions_grades)
+#   Get all assignments grades
+assignment_grade_blueprint.route('/all/grade',methods=['GET'])(get_all_submissions_grades)
 
 #   Update assignment grade by id
-assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['PUT'])(update_assignment_submission_grade)
+assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['PUT'])(update_submission_grade_by_id)
 
 #   Delete assignment grade by id
-assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['DELETE'])(delete_assignment_submission_grade)
+assignment_grade_blueprint.route('/<int:submission_id>/grade',methods=['DELETE'])(delete_submission_grade_by_id)
 
 
 # Student role
 
 #   Get assignment grade by id
-assignment_grade_blueprint.route('/grade',methods=['GET'])(get_assignment_submission_grade)
+assignment_grade_blueprint.route('/grade',methods=['GET'])(get_submission_grade_by_id)
 
 
