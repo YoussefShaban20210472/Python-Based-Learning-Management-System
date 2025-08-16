a46d68f5593ed0aec17195b5916a1869f2573127
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 15:20:58 +0300

    Update quizService.py

--- Flask/app/service/quizService.py
+++ Flask/app/service/quizService.py
@@ -4,7 +4,8 @@ import inspect
 
 #   Add new quiz
 def add_quiz(course_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get quiz by id
@@ -19,7 +20,8 @@ def get_all_quizzes(course_id):
 
 #   Update quiz by id
 def update_quiz_by_id(quiz_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'quiz_id': quiz_id}
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'quiz_id': quiz_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Delete quiz by id
