77aa98b80bf2efe6850ec05f2cf6ca276978f0a5
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:05:30 +0300

    Create questionBankService.py

---
+++ Flask/app/service/questionBankService.py
@@ -0,0 +1,29 @@
+from flask import  request
+from app.Repository.Repository import  execute
+import inspect
+
+#   Add new question bank
+def add_bank_question(course_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Get question bank by id
+def get_bank_question_by_id(question_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'question_id': question_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Get all questions bank
+def get_bank_questions(course_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Update question bank by id
+def update_bank_question_by_id(question_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'question_id': question_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+
+#   Delete question bank by id
+def delete_bank_question_by_id(question_id):
+    data= {'actor_id': getattr(request, "actor_id", None), 'question_id': question_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
