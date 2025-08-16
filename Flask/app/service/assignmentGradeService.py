ed8e27a53c881eb9d6a6c01675661c8e8ceb1697
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 15:20:37 +0300

    Update assignmentGradeService.py

--- Flask/app/service/assignmentGradeService.py
+++ Flask/app/service/assignmentGradeService.py
@@ -4,27 +4,29 @@ import inspect
 
 
 #   Add assignment grade
-def add_assignment_submission_grade(submission_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id}
+def add_submission_grade(submission_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get assignment grade by id
-def get_assignment_submission_grade(submission_id):
+def get_submission_grade_by_id(submission_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
-#   Get assignment grades
-def get_assignment_submissions_grades(course_id):
+#   Get all assignments grades
+def get_all_submissions_grades(course_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Update assignment grade by id
-def update_assignment_submission_grade(submission_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id}
+def update_submission_grade_by_id(submission_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Delete assignment grade by id
-def delete_assignment_submission_grade(submission_id):
+def delete_submission_grade_by_id(submission_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'submission_id': submission_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
