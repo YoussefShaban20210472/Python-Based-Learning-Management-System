9adf2457ecba816523843f99ec52de187b001dbb
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:32 +0300

    Update quizGradeController.py

--- Flask/app/controller/quizGradeController.py
+++ Flask/app/controller/quizGradeController.py
@@ -1,24 +1,23 @@
 from flask import Blueprint
+from app.service.quizGradeService import *
 
 quiz_grade_blueprint = Blueprint('quiz_grade', __name__)
 
-def demo(course_id=0,quiz_id=0,id=0):
-    return 'demo'
 
 
 # Instructor role
 
 #   Get quiz grade by id
-quiz_grade_blueprint.route('/attempt/<int:id>/grade',methods=['GET'])(demo)
+quiz_grade_blueprint.route('/attempt/<int:id>/grade',methods=['GET'])(get_quiz_attempt_grade)
 
 #   Get quiz grades
-quiz_grade_blueprint.route('/attempt/all/grade',methods=['GET'])(demo)
+quiz_grade_blueprint.route('/attempt/all/grade',methods=['GET'])(get_quizzes_attempts_grades)
 
 
 
 # Student role
 
 #   Get quiz grade by using authenticated student
-quiz_grade_blueprint.route('/attempt/grade',methods=['GET'])(demo)
+quiz_grade_blueprint.route('/attempt/grade',methods=['GET'])(get_my_quiz_attempt_grade())
 
 
