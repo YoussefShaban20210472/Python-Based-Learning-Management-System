43a8366d975315202489c4f579fa47d44a91d146
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:13 +0300

    Update mediaFileController.py

--- Flask/app/controller/mediaFileController.py
+++ Flask/app/controller/mediaFileController.py
@@ -1,32 +1,31 @@
 from flask import Blueprint
+from app.service.mediaFileService import *
 
 media_file_blueprint = Blueprint('media_file', __name__)
 
-def demo(course_id=0,id=0):
-    return 'demo'
 
 
 
 # Instructor role
 
 #   Add new media file
-media_file_blueprint.route('/',methods=['POST'])(demo)
+media_file_blueprint.route('/',methods=['POST'])(add_media_file)
 
 #   Get media file by id
-media_file_blueprint.route('/<int:id>',methods=['GET'])(demo)
+media_file_blueprint.route('/<int:id>',methods=['GET'])(get_media_file_by_id)
 
 #   Get all media files
-media_file_blueprint.route('/all',methods=['GET'])(demo)
+media_file_blueprint.route('/all',methods=['GET'])(get_all_media_files)
 
 #   Delete media file by id
-media_file_blueprint.route('/<int:id>',methods=['DELETE'])(demo)
+media_file_blueprint.route('/<int:id>',methods=['DELETE'])(delete_media_file_by_id)
 
 
 # Student role
 
 #   Get media_file by id
-media_file_blueprint.route('/<int:id>',methods=['GET'])(demo)
+media_file_blueprint.route('/<int:id>',methods=['GET'])(get_media_file_by_id)
 
 #   Get all media_files
-media_file_blueprint.route('/all',methods=['GET'])(demo)
+media_file_blueprint.route('/all',methods=['GET'])(get_all_media_files)
 
