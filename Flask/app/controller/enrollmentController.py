7b5c6067c5dd78b251a626d3d719598db0aff72d
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:08 +0300

    Update enrollmentController.py

--- Flask/app/controller/enrollmentController.py
+++ Flask/app/controller/enrollmentController.py
@@ -1,32 +1,31 @@
 from flask import Blueprint
-
+from app.service.enrollmentService import *
 enrollment_blueprint = Blueprint('enrollment', __name__)
 
-def demo(course_id=0, id=0):
-    return 'demo'
+
 
 
 # Instructor role
 
 #   Get all enrollments
-enrollment_blueprint.route('/all',methods=['GET'])(demo)
+enrollment_blueprint.route('/all',methods=['GET'])(get_all_enrollments)
 
 #   Get enrollment by id
-enrollment_blueprint.route('/<int:id>',methods=['GET'])(demo)
+enrollment_blueprint.route('/<int:enrollment_id>',methods=['GET'])(get_enrollment_by_id)
 
 #   Confirm enrollment by id
-enrollment_blueprint.route('/<int:id>',methods=['PUT'])(demo)
+enrollment_blueprint.route('/<int:enrollment_id>',methods=['PUT'])(update_enrollment_by_id)
 
 
 
 # Student role
 
 #   Add new enrollment
-enrollment_blueprint.route('/',methods=['POST'])(demo)
+enrollment_blueprint.route('/',methods=['POST'])(add_enrollment)
 
 #   Get enrollment by using authenticated student
-enrollment_blueprint.route('/',methods=['GET'])(demo)
+enrollment_blueprint.route('/',methods=['GET'])(get_my_enrollment)
 
 #   Delete enrollment by using authenticated student
-enrollment_blueprint.route('/',methods=['DELETE'])(demo)
+enrollment_blueprint.route('/',methods=['DELETE'])(delete_my_enrollment)
 
