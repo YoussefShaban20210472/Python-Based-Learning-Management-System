c74a9f1e0ee3326f075327ff240e97e996bd3e79
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:02 +0300

    Update assignmentSubmissionController.py

--- Flask/app/controller/assignmentSubmissionController.py
+++ Flask/app/controller/assignmentSubmissionController.py
@@ -1,29 +1,25 @@
 from flask import Blueprint
+from app.service.assignmentSubmissionService import *
 
 assignment_submission_blueprint = Blueprint('assignment_submission', __name__)
-
-def demo(course_id=0,assignment_id=0,id=0):
-    return 'demo'
-
-
 
 # Instructor role
 
 #   Get assignment submission by id
-assignment_submission_blueprint.route('/<int:id>',methods=['GET'])(demo)
+assignment_submission_blueprint.route('/<int:submission_id>',methods=['GET'])(get_submission_by_id)
 
 #   Get all assignment submissions
-assignment_submission_blueprint.route('/all',methods=['GET'])(demo)
+assignment_submission_blueprint.route('/all',methods=['GET'])(get_all_submissions)
 
 
 
 # Student role
 
 #   Add assignment submission
-assignment_submission_blueprint.route('/',methods=['POST'])(demo)
+assignment_submission_blueprint.route('/',methods=['POST'])(add_submission)
 
 #   Get assignment submission by using authenticated user
-assignment_submission_blueprint.route('/',methods=['GET'])(demo)
+assignment_submission_blueprint.route('/',methods=['GET'])(get_my_submission)
 
 #   Delete assignment submission by using authenticated user
-assignment_submission_blueprint.route('/',methods=['DELETE'])(demo)
+assignment_submission_blueprint.route('/',methods=['DELETE'])(delete_my_submission)
