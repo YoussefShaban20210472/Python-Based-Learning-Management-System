93f1ef727100256f55743e4af90f7752d35cf5cc
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:17 +0300

    Update progressController.py

--- Flask/app/controller/progressController.py
+++ Flask/app/controller/progressController.py
@@ -1,13 +1,11 @@
 from flask import Blueprint
+from app.service.progressService import *
 
 progress_blueprint = Blueprint('progress', __name__)
-
-def demo(course_id=0):
-    return 'demo'
 
 
 
 # Instructor / Student role
 
 #  Get progress
-progress_blueprint.route('',methods=['GET'])(demo)
+progress_blueprint.route('',methods=['GET'])(get_progress)
