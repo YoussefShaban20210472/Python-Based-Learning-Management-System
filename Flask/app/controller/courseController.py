a091a0b1c3da3f67d34d2c416e5ed1570463b238
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 05:02:06 +0300

    Update courseController.py

--- Flask/app/controller/courseController.py
+++ Flask/app/controller/courseController.py
@@ -1,31 +1,29 @@
 from flask import Blueprint
+from app.service.courseService import *
 
 course_blueprint = Blueprint('course', __name__)
-
-def demo(id =0):
-    return 'demo'
 
 
 # Instructor role
 
 #   Add new course
-course_blueprint.route('/',methods=['POST'])(demo)
+course_blueprint.route('/',methods=['POST'])(add_course)
 
 #   Get course by id
-course_blueprint.route('/<int:id>',methods=['GET'])(demo)
+course_blueprint.route('/<int:course_id>',methods=['GET'])(get_course_by_id)
 
 #   Update course by id
-course_blueprint.route('/<int:id>',methods=['PUT'])(demo)
+course_blueprint.route('/<int:course_id>',methods=['PUT'])(update_course_by_id)
 
 #   Delete course by id
-course_blueprint.route('/<int:id>',methods=['DELETE'])(demo)
+course_blueprint.route('/<int:course_id>',methods=['DELETE'])(delete_course_by_id)
 
 
 # Student role
 
 #   Get all courses
-course_blueprint.route('/all',methods=['GET'])(demo)
+course_blueprint.route('/all',methods=['GET'])(get_all_courses)
 
 #   Get course by id
-course_blueprint.route('/<int:id>',methods=['GET'])(demo)
+course_blueprint.route('/<int:course_id>',methods=['GET'])(get_course_by_id)
 
