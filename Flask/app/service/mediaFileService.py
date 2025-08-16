913252f38c2a0943e849f7422f71323a169b1705
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-16 15:20:53 +0300

    Update mediaFileService.py

--- Flask/app/service/mediaFileService.py
+++ Flask/app/service/mediaFileService.py
@@ -4,21 +4,22 @@ import inspect
 
 #   Add new media file
 def add_media_file(course_id):
-    data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get media file by id
-def get_media_file_by_id(media_file_id):
+def get_course_media_file_by_id(media_file_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get all media files
-def get_all_media_files(course_id):
+def get_all_course_media_files(course_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Delete media file by id
-def delete_media_file_by_id(media_file_id):
+def delete_course_media_file_by_id(media_file_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
