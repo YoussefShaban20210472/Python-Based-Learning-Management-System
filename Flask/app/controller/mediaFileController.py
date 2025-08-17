96a4e7351770c31324d7c778992ad4ae79b30627
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-17 04:17:44 +0300

    Update mediaFileController.py

--- Flask/app/controller/mediaFileController.py
+++ Flask/app/controller/mediaFileController.py
@@ -9,7 +9,7 @@ media_file_blueprint = Blueprint('media_file', __name__)
 # Instructor role
 
 #   Add new media file
-media_file_blueprint.route('/',methods=['POST'])(add_media_file)
+media_file_blueprint.route('/',methods=['POST'])(add_course_media_file)
 
 #   Get media file by id
 media_file_blueprint.route('/<int:media_file_id>',methods=['GET'])(get_course_media_file_by_id)
@@ -18,7 +18,7 @@ media_file_blueprint.route('/<int:media_file_id>',methods=['GET'])(get_course_me
 media_file_blueprint.route('/all',methods=['GET'])(get_all_course_media_files)
 
 #   Delete media file by id
-media_file_blueprint.route('/<int:id>',methods=['DELETE'])(delete_course_media_file_by_id)
+media_file_blueprint.route('/<int:media_file_id>',methods=['DELETE'])(delete_course_media_file_by_id)
 
 
 # Student role
