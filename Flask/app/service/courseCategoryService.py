9b13dbf12f46d3eb3e6d62ad33661e6b258d9aa7
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-17 04:17:58 +0300

    Create courseCategoryService.py

---
+++ Flask/app/service/courseCategoryService.py
@@ -0,0 +1,30 @@
+from flask import  request
+from app.Repository.Repository import  execute
+import inspect
+
+
+#   Add new course category
+def add_course_category(course_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Get all course categories
+def get_all_course_categories(course_id):
+    data = {'actor_id': getattr(request, "actor_id", None), 'course_id': course_id}
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Update course category
+def update_course_category(course_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+#   Delete course category
+def delete_course_category(course_id):
+    data = request.get_json()
+    data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
+    return execute(inspect.currentframe().f_code.co_name, data)
+
+
+
