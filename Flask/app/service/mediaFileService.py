6ad39ac45d3c49370ed07806def37f8cd0620986
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-17 04:18:07 +0300

    Update mediaFileService.py

--- Flask/app/service/mediaFileService.py
+++ Flask/app/service/mediaFileService.py
@@ -3,13 +3,13 @@ from app.Repository.Repository import  execute
 import inspect
 
 #   Add new media file
-def add_media_file(course_id):
+def add_course_media_file(course_id):
     data = request.get_json()
     data.update({'actor_id': getattr(request, "actor_id", None), 'course_id': course_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get media file by id
-def get_course_media_file_by_id(media_file_id):
+def get_course_media_file_by_id(course_id,media_file_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
@@ -19,7 +19,7 @@ def get_all_course_media_files(course_id):
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Delete media file by id
-def delete_course_media_file_by_id(media_file_id):
+def delete_course_media_file_by_id(course_id,media_file_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'media_file_id': media_file_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
