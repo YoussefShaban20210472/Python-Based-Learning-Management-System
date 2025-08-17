0a46070403f8aeb0298b8a3e476cff82a04f2f67
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-17 04:18:05 +0300

    Update lessonService.py

--- Flask/app/service/lessonService.py
+++ Flask/app/service/lessonService.py
@@ -9,7 +9,7 @@ def add_lesson(course_id):
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Get lesson by id
-def get_lesson_by_id(lesson_id):
+def get_lesson_by_id(course_id,lesson_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'lesson_id': lesson_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
@@ -19,13 +19,13 @@ def get_all_lessons(course_id):
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Update lesson by id
-def update_lesson_by_id(lesson_id):
+def update_lesson_by_id(course_id,lesson_id):
     data = request.get_json()
     data.update({'actor_id': getattr(request, "actor_id", None), 'lesson_id': lesson_id})
     return execute(inspect.currentframe().f_code.co_name, data)
 
 #   Delete lesson by id
-def delete_lesson_by_id(lesson_id):
+def delete_lesson_by_id(course_id,lesson_id):
     data= {'actor_id': getattr(request, "actor_id", None), 'lesson_id': lesson_id}
     return execute(inspect.currentframe().f_code.co_name, data)
 
