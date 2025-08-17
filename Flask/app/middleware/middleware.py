95f88389c6d1f814e26fa6a415c1fa952cb5552b
YoussefShaban20210472 <102224925+YoussefShaban20210472@users.noreply.github.com>
2025-08-17 04:17:52 +0300

    Update middleware.py

--- Flask/app/middleware/middleware.py
+++ Flask/app/middleware/middleware.py
@@ -1,12 +1,11 @@
 from flask import  request
-from app.Repository.Repository import login
-
+from app.Repository.Repository import  execute
 
 def authenticate():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
         return
     data = {'email': auth.username, 'password': auth.password}
-    result,code = login(data)
+    result,code = execute('login',data)
     if code == 200:
         request.actor_id = result[0]['id']
